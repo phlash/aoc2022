566e88ddb5edf30b773a31f14b5a05302b37d728
Phil Ashby <phil.github@ashbysoft.com>
2022-12-01 17:12:40 +0000

    day 1: part 1

---
+++ day1/main.scala
@@ -0,0 +1,26 @@
+// Calorie counting - simple line-by-line input, parsing integers and adding up..
+import scala.io.StdIn
+import scala.collection.mutable.ArrayBuffer
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    var elfs = ArrayBuffer.empty[Int]
+    elfs += 0
+    var elf : Int = 0
+    var line = StdIn.readLine()
+    while (line != null) {
+      if (line.length>0) {
+        // value? add to current elf count
+        elfs.update(elf, elfs(elf) + line.toInt)
+      } else {
+        // next elf
+        elfs += 0
+        elf += 1
+      }
+      line = StdIn.readLine()
+    }
+    println(elfs)
+    val max = elfs.max
+    println("Max:"+max)
+  }
+}
