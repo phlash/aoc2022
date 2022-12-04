516277d0479edfc0cd605ceec5eb4db1bf21b3b5
Phil Ashby <phil.github@ashbysoft.com>
2022-12-04 11:05:49 +0000

    day 4: part 1

---
+++ day4/main.scala
@@ -0,0 +1,35 @@
+// Camp cleanup
+import scala.io.StdIn
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    var line = StdIn.readLine()
+    var count: Int = 0
+    while (line!=null) {
+      // split and parse each pair of ranges
+      val pair = line.split(',')
+      if (pair.length!=2) {
+        println("oops: line not a pair?: "+line)
+        return
+      }
+      val elf1 = pair(0).split('-')
+      val elf2 = pair(1).split('-')
+      if (elf1.length!=2 || elf2.length!=2) {
+        println("oops: not a range?: "+elf1+","+elf2)
+        return
+      }
+      val r1b = elf1(0).toInt
+      val r1e = elf1(1).toInt
+      val r2b = elf2(0).toInt
+      val r2e = elf2(1).toInt
+      println("parsed: "+r1b+','+r1e+','+r2b+','+r2e)
+      // containment check!
+      if (r1b<=r2b && r1e>=r2e)
+        count += 1
+      else if (r2b<=r1b && r2e>=r1e)
+        count += 1
+      line = StdIn.readLine()
+    }
+    println("Count: "+count)
+  }
+}
