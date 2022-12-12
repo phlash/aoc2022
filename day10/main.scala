4c2f68897602324b1a067dedc35f94ea0e638387
Phil Ashby <phil.github@ashbysoft.com>
2022-12-12 19:47:28 +0000

    day 10: part 2

--- day10/main.scala
+++ day10/main.scala
@@ -4,11 +4,21 @@ import scala.io.StdIn
 object Main {
   def check(time:Int, xreg:Int, sum:Int): Int =
       if (time==20 || ((time-20)%40)==0) {
-        println("time:"+time+", x:"+xreg+", ss:"+(time*xreg))
+        //println("time:"+time+", x:"+xreg+", ss:"+(time*xreg))
         sum + time*xreg
       } else {
         sum
       }
+
+  def render(time:Int, xreg:Int): Unit = {
+    val pix = time%40
+    if (pix>xreg-2 && pix<xreg+2)
+      print('#')
+    else
+      print('.')
+    if (39==pix)
+      println()
+  }
 
   def main(args: Array[String]): Unit = {
     var xreg = 1
@@ -17,11 +27,14 @@ object Main {
     var line = StdIn.readLine()
     while (line != null) {
       if (line.startsWith("noop")) {
+        render(time, xreg)
         time += 1
         part1 = check(time, xreg, part1)
       } else {
+        render(time, xreg)
         time += 1
         part1 = check(time, xreg, part1)
+        render(time, xreg)
         time += 1
         part1 = check(time, xreg, part1)
         xreg += line.split(" ")(1).toInt
