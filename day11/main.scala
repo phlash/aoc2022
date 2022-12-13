b796b8b2e909bf5cbab1bebb8fa9d368bb374a67
Phil Ashby <phil.github@ashbysoft.com>
2022-12-13 03:50:27 +0000

    day 11: part 2

--- day11/main.scala
+++ day11/main.scala
@@ -1,57 +1,59 @@
 // Monkey in the Middle - logic simulation..
 
-class Monkey(var items:List[Int], val op:Function1[Int,Int], val test:Function1[Int,Int]) {
-  var count: Int = 0
+class Monkey(var items:List[Long], val op:Function1[Long,Long], val div:Long, val yeah:Int, val nah:Int) {
+  var count: Long = 0
   override def toString = "Monkey(count="+count+", items="+items.toString+")"
 }
 object Monkey {
   def test =
     List(
-      new Monkey(List(79, 98), x => x*19, x => if ((x%23)==0) 2 else 3),
-      new Monkey(List(54, 65, 75, 74), x => x+6, x => if ((x%19)==0) 2 else 0),
-      new Monkey(List(79, 60, 97), x => x*x, x => if ((x%13)==0) 1 else 3),
-      new Monkey(List(74), x => x+3, x => if ((x%17)==0) 0 else 1)
+      new Monkey(List(79, 98), x => x*19, 23, 2, 3),
+      new Monkey(List(54, 65, 75, 74), x => x+6, 19, 2, 0),
+      new Monkey(List(79, 60, 97), x => x*x, 13, 1, 3),
+      new Monkey(List(74), x => x+3, 17, 0, 1)
     )
   def input =
     List(
-      new Monkey(List(91, 54, 70, 61, 64, 64, 60, 85), x => x*13, x => if ((x%2)==0) 5 else 2),
-      new Monkey(List(82), x => x+7, x => if ((x%13)==0) 4 else 3),
-      new Monkey(List(84, 93, 70), x => x+2, x => if ((x%5)==0) 5 else 1),
-      new Monkey(List(78, 56, 85, 93), x => x*2, x => if ((x%3)==0) 6 else 7),
-      new Monkey(List(64, 57, 81, 95, 52, 71, 58), x => x*x, x => if ((x%11)==0) 7 else 3),
-      new Monkey(List(58, 71, 96, 58, 68, 90), x => x+6, x => if ((x%17)==0) 4 else 1),
-      new Monkey(List(56, 99, 89, 97, 81), x => x+1, x => if ((x%7)==0) 0 else 2),
-      new Monkey(List(68, 72), x => x+8, x => if ((x%19)==0) 6 else 0)
+      new Monkey(List(91, 54, 70, 61, 64, 64, 60, 85), x => x*13, 2, 5, 2),
+      new Monkey(List(82), x => x+7, 13, 4, 3),
+      new Monkey(List(84, 93, 70), x => x+2, 5, 5, 1),
+      new Monkey(List(78, 56, 85, 93), x => x*2, 3, 6, 7),
+      new Monkey(List(64, 57, 81, 95, 52, 71, 58), x => x*x, 11, 7, 3),
+      new Monkey(List(58, 71, 96, 58, 68, 90), x => x+6, 17, 4, 1),
+      new Monkey(List(56, 99, 89, 97, 81), x => x+1, 7, 0, 2),
+      new Monkey(List(68, 72), x => x+8, 19, 6, 0)
     )
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
     val monkeys = if (args.length>0 && args(0).startsWith("input")) Monkey.input else Monkey.test
+    val div = if (args.length>1 && args(1).startsWith("part2")) 1 else 3
     println(monkeys)
-    // Play 20 rounds of keep away..
-    for (r <- 1 to 20) {
-      println("ROUND "+r)
+    var lcm:Long = 1
+    for (m <- monkeys)
+      lcm = lcm*m.div
+    // Play required rounds of keep away..
+    for (r <- 1 to (if (div>1) 20 else 10000)) {
       // for each monkey in turn..
-      var i = 0
       for (m <- monkeys) {
-        println("  Monkey "+i)
-        i += 1
         // inspect each item, apply logic
         for (i <- m.items) {
           m.count += 1
-          val w = m.op(i)/3
-          val t = m.test(w)
+          val w = (m.op(i)/div) % lcm
+          val t = if ((w%m.div)==0) m.yeah else m.nah
           monkeys(t).items = monkeys(t).items :+ w
-          println("    "+i+" => "+w+" => "+t)
         }
         m.items = List()
       }
-      println(monkeys)
+      if (1==r || 20==r || (r%1000)==0) {
+        print("ROUND "+r+" ")
+        println(monkeys)
+      }
     }
     // find two highest counts
-    var c1 = 0
-    var c2 = 0
+    var c1:Long = 0
+    var c2:Long = 0
     for (m <- monkeys) {
       if (m.count > c1) {
         c2 = c1
