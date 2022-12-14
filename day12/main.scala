d114f90d0e29430504d58f0327ebbf0bf4a96d4b
Phil Ashby <phil.github@ashbysoft.com>
2022-12-14 04:24:00 +0000

    day 12: part 1 - djikstra FTW

---
+++ day12/main.scala
@@ -0,0 +1,85 @@
+// Hill Climbing, aka shortest path algorithm..
+import scala.io.StdIn
+import scala.collection.mutable.ArrayBuffer
+
+class Cell(val height:Int, var cost:Int = Int.MaxValue) {}
+
+object Main {
+  def shortest(map:ArrayBuffer[ArrayBuffer[Cell]], pos:(Int,Int), top:(Int,Int), path:List[(Int,Int)]): Int = {
+    // the rules
+    // - can ONLY move vertically/horiontally
+    // - can ONLY ascend one unit
+    // - can descend
+
+    if (pos==top) { // Yay, we made it!
+      println(pos+":top@"+path.length+"="+path)
+      return path.length
+    }
+    // update lowest cost to this cell
+    val (x,y) = pos
+    map(y)(x).cost = path.length
+    // filter & sort possible directions in steepness
+    val hgt = map(y)(x).height
+    val dirs = List((x-1,y),(x+1,y),(x,y-1),(x,y+1)).filter(
+        (d) => {
+          val (tx,ty) = d
+          val o = tx>=0 && tx<map(0).length && ty>=0 && ty<map.length
+          val p = o && !path.contains(d)
+          val h = p && map(ty)(tx).height<hgt+2
+          val c = h && map(ty)(tx).cost>path.length+1 // Djikstra magic - avoid more expensive options
+          c
+        }
+      ).sortWith(
+        (a,b) => {
+          val (ax,ay) = a
+          val (bx,by) = b
+          val ah = map(ay)(ax).height
+          val bh = map(by)(bx).height
+          ah>bh
+        }
+      )
+    // recursive search..
+    var d = -1
+    for (nxt <- dirs) {
+      var td = shortest(map, nxt, top, path :+ pos)
+      if (d<0 || (td>=0 && td<d))
+        d = td
+    }
+    return d
+  }
+
+  def main(args: Array[String]): Unit = {
+    var map: ArrayBuffer[ArrayBuffer[Cell]] = ArrayBuffer()
+    var line = StdIn.readLine()
+    var width = 0
+    var x = 0
+    var y = 0
+    var start = (0,0)
+    var top = (0,0)
+    while (line != null) {
+      if (width<1) {
+        width = line.length
+      } else if (width != line.length) {
+        println("oops: map width changed?")
+        return
+      }
+      var row:ArrayBuffer[Cell] = new ArrayBuffer(width)
+      x = 0
+      for (c <- line) {
+        c match {
+          case 'S' => { start = (x,y); row = row :+ new Cell(0) }
+          case 'E' => { top = (x,y); row = row :+ new Cell(25) }
+          case _ => row = row :+ new Cell(c-'a')
+        }
+        x += 1
+      }
+      map = map :+ row
+      y += 1
+      line = StdIn.readLine()
+    }
+    println(f"map(${width}x${y}) start=${start} top=${top}")
+
+    // Go find that route!
+    println("shortest path="+shortest(map, start, top, List()))
+  }
+}
