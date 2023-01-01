a50013b0066b940b23b80374691f8216e9f882ee
Phil Ashby <phil.github@ashbysoft.com>
2023-01-02 09:45:39 +1300

    day 20: part 2

--- day20/main.scala
+++ day20/main.scala
@@ -4,51 +4,74 @@ import scala.collection.mutable.ArrayBuffer
 
 object Main {
   def main(args: Array[String]): Unit = {
-    var din: ArrayBuffer[(Int,Int)] = ArrayBuffer()
+    var din: ArrayBuffer[(Long,Long)] = ArrayBuffer()
     var line = StdIn.readLine()
     var pos = 0
     while (line != null) {
-      din = din :+ (pos,line.toInt)
+      din = din :+ (pos.toLong,line.toLong*811589153L)
       pos += 1
       line = StdIn.readLine()
     }
     println(f"input: ${din}")
     var dout = din.clone
-    for (p <- din) {
-      // find it..
-      var pos = dout.indexOf(p)
-      assert(pos>=0 && pos<dout.length)
-      // determine step count & direction..
-      var stp = p._2.abs
-      //print(f"stp=${stp} ")
-      if (stp!=0) {
-        val dir = p._2/stp
-        //print(f"dir=${dir} ")
-        // move value..
-        while (stp>0) {
-          var npos = pos+dir
-          if (npos<0)
-            npos = dout.length-1
-          if (npos>dout.length-1)
-            npos = 0
-          //print(f"${pos}=>${npos} ")
-          // swap values..
-          val t = dout(npos)
-          dout(npos) = dout(pos)
-          dout(pos) = t
-          stp -= 1
-          pos = npos
+    val dlen = dout.length
+    for (i <- 0 until 10) {
+      println(i)
+      for (p <- din) {
+        // find it..
+        var pos = dout.indexOf(p)
+        assert(pos>=0)
+        // determine step count (modulo array size!) & direction..
+        var stp = p._2.abs % (dlen-1)
+        //print(f"stp=${p._2}/${stp} ")
+        if (stp!=0) {
+          val dir = if (p._2>0) 1 else -1
+          //print(f"dir=${dir} ")
+          // move value..
+          while (stp>0) {
+            var npos = pos+dir
+            val tst = (pos,npos)
+            // special case, slide everything left, append.
+            if (tst == (1,0)) {
+                dout = (dout(0) +: dout.slice(2, dlen)) :+ p
+                npos = dlen-1
+            }
+            // special case, slide most left, insert penultimate pos.
+            else if (tst == (0,-1)) {
+              dout = (dout.slice(1, dlen-1) :+ p) :+ dout(dlen-1)
+              npos = dlen-2
+            }
+            // special case, slide everything right, prepend.
+            else if (tst == (dlen-2,dlen-1)) {
+              dout = p +: (dout.slice(0,dlen-2) :+ dout(dlen-1))
+              npos = 0
+            }
+            // special case, slide most right, insert pos 1
+            else if (tst == (dlen-1,dlen)) {
+              dout = dout(0) +: (p +: dout.slice(1,dlen-1))
+              npos = 1
+            }
+            // standard case, swap
+            else {
+              dout(pos) = dout(npos)
+              dout(npos) = p
+            }
+            //println(f"stp=${stp} p=${p} ${dout}")
+            assert(dlen==dout.length)
+            stp -= 1
+            pos = npos
+          }
         }
+        //println(f": ${dout}")
       }
-      //println(f": ${dout}")
     }
     // calculate co-ordinates
     val zpos = dout.indexWhere(_._2==0)
     println(f"zero@${zpos}")
-    val part1 =
+    val part2 =
       dout((zpos+1000)%dout.length)._2 +
       dout((zpos+2000)%dout.length)._2 +
       dout((zpos+3000)%dout.length)._2
-    println(f"part1: ${part1}")
+    println(f"part2: ${part2}")
   }
 }
