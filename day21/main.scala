8e2487e09e5f8b7eda9a1a3d21ca91e532a595be
Phil Ashby <phil.github@ashbysoft.com>
2023-01-02 15:08:21 +1300

    day 21: part 2 - tree of objects

--- day21/main.scala
+++ day21/main.scala
@@ -2,35 +2,70 @@
 import scala.io.StdIn
 import scala.collection.mutable.HashMap
 
-object Main {
-  def doop(op: String, lhs: ()=>Long, rhs: ()=>Long): Long =
+class Monkey(val name: String, val lhs: Monkey, val rhs: Monkey, val op: String) {
+  var ish = false
+  override def toString = "M("+op+")"+ (if (lhs!=null) "L"+lhs.toString) + (if (rhs!=null) "R"+rhs.toString)
+  def find(): Boolean = {
+    ish = name == "humn" ||
+      (lhs!=null && lhs.find()) ||
+      (rhs!=null && rhs.find())
+    ish
+  }
+  def doop(): Long = {
     op match {
-      case "+" => lhs() + rhs()
-      case "-" => lhs() - rhs()
-      case "*" => lhs() * rhs()
-      case "/" => lhs() / rhs()
-      case _ => { assert(false); 0L }
+      case "+" => lhs.doop() + rhs.doop()
+      case "-" => lhs.doop() - rhs.doop()
+      case "*" => lhs.doop() * rhs.doop()
+      case "/" => lhs.doop() / rhs.doop()
+      case _ => op.toLong
     }
+  }
+  def solve(v: Long) {
+    assert(ish)
+    // calculate residual
+    val r = op match {
+      case "+" => if (lhs.ish) v - rhs.doop() else v - lhs.doop()
+      case "-" => if (lhs.ish) v + rhs.doop() else lhs.doop() - v
+      case "*" => if (lhs.ish) v / rhs.doop() else v / lhs.doop()
+      case "/" => if (lhs.ish) v * rhs.doop() else lhs.doop() / v
+      case _ => { println(f"part2: ${v}"); v }
+    }
+    // pass down..
+    if (lhs!=null && lhs.ish)
+      lhs.solve(r)
+    else if (rhs!=null && rhs.ish)
+      rhs.solve(r)
+  }
+}
 
+object Main {
+  def gentree(name: String, loadmap: HashMap[String,Array[String]]): Monkey = {
+    val toks = loadmap(name)
+    toks.length match {
+      case 2 => new Monkey(name,null,null,toks(1))
+      case 4 => new Monkey(name,gentree(toks(1),loadmap),gentree(toks(3),loadmap),toks(2))
+      case _ => sys.error(f"${toks}")
+    }
+  }
   def main(args: Array[String]): Unit = {
-    // We use a map, from monkey name to function returning value
-    val monkeys: HashMap[String,() => Long] = HashMap()
+    val loadmap: HashMap[String,Array[String]] = HashMap()
     var line = StdIn.readLine()
     while (line != null) {
-      // either:
-      // <monkey>: <value> => function returns value
-      // or:
-      // <monkey>: <name> <op> <name> => function applies op to other map entries
       val toks = line.split(" ")
       val name = toks(0).substring(0,toks(0).length-1)
-      toks.length match {
-        case 2 => monkeys(name) = () => toks(1).toLong
-        case 4 => monkeys(name) = () => doop(toks(2), monkeys(toks(1)), monkeys(toks(3)))
-        case _ => assert(false)
-      }
+      loadmap(name) = toks
       line = StdIn.readLine()
     }
-    val part1 = monkeys("root")()
+    // process loaded map into a tree of Monkeys
+    val root = gentree("root", loadmap)
+    // calculate final value
+    val part1 = root.doop()
     println(f"part1: ${part1}")
+    // find path to humn
+    root.find()
+    // solve for equality at root..
+    val lhs = root.lhs
+    val rhs = root.rhs
+    if (lhs.ish) lhs.solve(rhs.doop()) else rhs.solve(lhs.doop())
   }
 }
